cf1ef2bd554b6fe4090ccb99acbcb2c843622c06
Karpunin Pasha <amedomaro2@gmail.com>
2025-06-12 15:04:11 +0300

    123

---
+++ classes/shared_lib.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package    assignsubmission_ctsubmission
+ * @category   NED
+ * @copyright  2022 NED {@link http://ned.ca}
+ * @author     NED {@link http://ned.ca}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace availability_ctcompletion;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Class shared_lib
+ *
+ * @package assignsubmission_ctsubmission
+ */
+class shared_lib extends \local_ned_controller\shared\base_class {
+    use \local_ned_controller\shared\base_trait;
+
+}
+
+shared_lib::init();
